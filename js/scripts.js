eb324bf484c493b9df42f3f92f1a304e2b37ca5e
Alex Sweeney <ajsweeney2324@gmail.com>
2023-03-30 09:39:46 -0500

    added main functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,41 @@
 
 
+$(document).ready(function() {
+// Add event listeners for the add button and the enter key
+// to add that task to the list and clear the field
+
 $('button').click(
     function(){
-        var toAdd = $('input[name=toDoTask').val();
+        var toAdd = $('input[name=toDoTask]').val();
         $('ul').append('<li>'+toAdd+'</li>');
+        $('input[name=toDoTask]').val('');
     }
 )
 
+$('input[name=toDoTask]').keypress(function(event){
+    if(event.keyCode === 13) {
+        event.preventDefault();
+        var toAdd = $('input[name=toDoTask]').val();
+        $('ul').append('<li>' + toAdd + '</li>');
+        $(this).val('');
+    }
+})
 
 
 
+$('ul').on('click', 'li', function() {
+    $(this).toggleClass('strike');
+});
 
-
-
-li.on('click', function(){
-    li.addClass('strike');
+$('ul').on('dblclick', 'li', function() {
+    $(this).addClass('delete');
 });
 
 
+
+$('ul').sortable({
+    axis: 'y',
+    containment: 'parent'
+});
+
+});
